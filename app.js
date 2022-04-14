cfda4b32dfe05b6e7b734b7dd1a2d83c00f7f0da
ajay-wh1 <ajaya@warehousing1.com>
2022-04-14 19:00:15 +0545

    Initial setup

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello this is application (vpc-test1) created for vpv testing, deployed on faragate"));
+
+
+
+app.get("/health", (req, res) => {
+  res.status(200);
+  res.send("healthy");
+});
+
+app.listen(3000, () => {
+  console.log("App listening on port 3000!, retry");
+});
