5923c1617c9206a13f7d5c3c91dfb2581cd7f46c
ajay-wh1 <ajaya@warehousing1.com>
2022-04-15 10:05:10 +0545

    axios on docker

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const axios = require("axios");
 
 app.get("/", (req, res) => res.send("Hello this is application (vpc-test1) created for vpv testing, deployed on faragate"));
 
@@ -14,16 +15,8 @@ app.listen(3000, () => {
   console.log("App listening on port 3000!, retry");
 });
 
-
-const http = require('http');
-
-app.get("/access_alb", (req, res) => {
-  response = http.get('http://internal-alb-vpc-test2-v3-948757348.ap-south-1.elb.amazonaws.com/', (res) => {
-    return res
-  }).on('error', (e) => {
-    console.error(`Got error: ${e.message}`);
-  });
-  res.status(200);
-  res.send(response)
+app.get("/access_alb",async (req, res) => {
+  const {data}=await axios.get("http://nlb-vpc-test2-5fa65a1afc079eb1.elb.ap-south-1.amazonaws.com")
+  res.send(data);
 });
 
