427156eba8d06cd97720a35e5e518afcf9225d0c
ajay-wh1 <ajaya@warehousing1.com>
2022-04-15 09:00:04 +0545

    hitting vpc-test2 internal alb

--- app.js
+++ app.js
@@ -13,3 +13,17 @@ app.get("/health", (req, res) => {
 app.listen(3000, () => {
   console.log("App listening on port 3000!, retry");
 });
+
+
+const http = require('http');
+
+app.get("/access_alb", (req, res) => {
+  response = http.get('http://internal-alb-vpc-test2-v3-948757348.ap-south-1.elb.amazonaws.com/', (res) => {
+    return res
+  }).on('error', (e) => {
+    console.error(`Got error: ${e.message}`);
+  });
+  res.status(200);
+  res.send(response)
+});
+
